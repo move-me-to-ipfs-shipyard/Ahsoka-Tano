df06dbe941970d19a53f96e6cf3b63cf82fba518
Ahsoka-Tano <Ahsoka-Tano@or-face-the-consequences>
2022-07-04 12:45:16 +0300

    my release is defined eniterly by commit hash :Clarkson we were supposed to be doing 100 miles a day - so far we've done like what? 18 yards?

--- program.sh
+++ program.sh
@@ -14,24 +14,13 @@ main(){
     -M -m Ahsoka-Tano.main
 }
 
-tag(){
-  COMMIT_HASH=$(git rev-parse --short HEAD)
-  COMMIT_COUNT=$(git rev-list --count HEAD)
-  TAG="$COMMIT_COUNT-$COMMIT_HASH"
-  git tag $TAG $COMMIT_HASH
-  echo $COMMIT_HASH
-  echo $TAG
-}
-
 jar(){
 
   rm -rf out/*.jar out/classes
-  COMMIT_HASH=$(git rev-parse --short HEAD)
-  COMMIT_COUNT=$(git rev-list --count HEAD)
   clojure \
     -X:Genie Genie.core/process \
     :main-ns Ahsoka-Tano.main \
-    :filename "\"out/Ahsoka-Tano-$COMMIT_COUNT-$COMMIT_HASH.jar\"" \
+    :filename "\"out/Ahsoka-Tano-$(git rev-parse --short HEAD).jar\"" \
     :paths '["src"]'
 }
 
