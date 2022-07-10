7aeb4d39b31d4d3d3ac6c4af1590ada1504a4834
Ahsoka-Tano <Ahsoka-Tano@or-face-the-consequences>
2022-07-10 21:10:41 +0300

    i revert "i revert "i revert "our bread is oats - water-away whole grain bread"""
    This reverts commit eb55a2e6942990049293d7e624bfca8d6e207b62.

--- src/Ahsoka_Tano/main.clj
+++ src/Ahsoka_Tano/main.clj
@@ -18,7 +18,7 @@
    [Ahsoka-Tano.pears]
    [Ahsoka-Tano.B12]
    [Ahsoka-Tano.salt]
-   [Ahsoka-Tano.oats]
+   [Ahsoka-Tano.bread]
    [Ahsoka-Tano.raisins])
   (:import
    (javax.swing JFrame WindowConstants JPanel JScrollPane JTextArea BoxLayout JEditorPane ScrollPaneConstants SwingUtilities JDialog)
@@ -68,7 +68,7 @@
    '[Ahsoka-Tano.pears]
    '[Ahsoka-Tano.B12]
    '[Ahsoka-Tano.salt]
-   '[Ahsoka-Tano.oats]
+   '[Ahsoka-Tano.bread]
    '[Ahsoka-Tano.raisins]
    '[Ahsoka-Tano.main]
    :reload))
@@ -192,7 +192,7 @@
             jpanel-pears (JPanel.)
             jpanel-B12 (JPanel.)
             jpanel-salt (JPanel.)
-            jpanel-oats (JPanel.)
+            jpanel-bread (JPanel.)
             jpanel-raisins (JPanel.)]
 
         (doto jtabbed-pane
@@ -200,11 +200,11 @@
           (.addTab "pears" jpanel-pears)
           (.addTab "B12" jpanel-B12)
           (.addTab "salt" jpanel-salt)
-          (.addTab "oats" jpanel-oats)
+          (.addTab "bread" jpanel-bread)
           (.addTab "raisins" jpanel-raisins)
-          (.setSelectedComponent jpanel-oats))
+          (.setSelectedComponent jpanel-bread))
 
-        (Ahsoka-Tano.oats/process {:jpanel-tab jpanel-oats
+        (Ahsoka-Tano.bread/process {:jpanel-tab jpanel-bread
                                :db-data-dirpath db-data-dirpath})
 
         (settings-process {:jpanel-tab jpanel-B12
